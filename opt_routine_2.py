bf366f4fc5112aecc23e65907fb9a37a5dba39ce
hamishmack1 <hamishmack01@gmail.com>
2023-08-13 22:47:10 +1000

    "model update"

--- opt_routine_2.py
+++ opt_routine_2.py
@@ -26,12 +26,11 @@ lines = lines[1:]
 
 length = int(len(lines) / 6)
 
-consumption = np.zeros((length, len(header) - 2, 1))
-generation = np.zeros((length, len(header) - 2, 1))
-grid_import = np.zeros((length, len(header) - 2, 1))
-grid_export = np.zeros((length, len(header) - 2, 1))
-bat_expend = np.zeros((length, len(header) - 2, 1))
-bat_soc = np.zeros((length, len(header) - 2, 1))
+forecast_data = np.zeros((length, 2, len(header) - 2))
+grid_import = np.zeros((length, len(header) - 2))
+grid_export = np.zeros((length, len(header) - 2))
+bat_expend = np.zeros((length, len(header) - 2))
+bat_soc = np.zeros((length, len(header) - 2))
 
 counter = 0
 for line in lines:
@@ -39,90 +38,66 @@ for line in lines:
     category = split_line[0]
     values = [float(x) for x in split_line[2:]]
 
-    for i in range(len(values)):
-        if category == "GC":
-            consumption[counter,i] = values[i]
-        elif category == "GG":
-            generation[counter, i] = values[i]
-        elif category == "GI":
-            grid_import[counter, i] = values[i]
-        elif category == "GE":
-            grid_export[counter, i] = values[i]
-        elif category == "BC":
-            bat_expend[counter, i] = values[i]
-        elif category == "SOC":
-            bat_soc[counter, i] = values[i]
-            if i == len(values) - 1:
-                counter += 1
+    if category == "GC":
+        forecast_data[counter, 0, :] = values
+    elif category == "GG":
+        forecast_data[counter, 1, :] = values
+    elif category == "GI":
+        grid_import[counter, :] = values
+    elif category == "GE":
+        grid_export[counter, :] = values
+    elif category == "BC":
+        bat_expend[counter, :] = values
+    elif category == "SOC":
+        bat_soc[counter, :] = values
+        counter += 1
 
 # Preparing data
 
-# Problem: Given current battery SOC, and forecast of demand and generation for the next week, predict todays optimal energy allocation decisions.
+# Problem: Given current battery SOC, and forecast of demand and generation, predict todays optimal energy allocation decisions.
 
 num_train_samples = int(0.5 * length)
 num_val_samples = int(0.25 * length)
 num_test_samples = length - num_train_samples - num_val_samples
 
-train_consumption = consumption[:num_train_samples]
-train_generation = generation[:num_train_samples]
+train_forecast = forecast_data[:num_train_samples]
 train_grid_import = grid_import[:num_train_samples]
 train_grid_export = grid_export[:num_train_samples]
-train_bat_expend = bat_expend[:num_train_samples]
+train_bat_expend = bat_expend[:num_train_samples] / 10
 
-val_consumption = consumption[num_train_samples:num_train_samples+num_val_samples]
-val_generation = generation[num_train_samples:num_train_samples+num_val_samples]
+val_forecast = forecast_data[num_train_samples:num_train_samples+num_val_samples]
 val_grid_import = grid_import[num_train_samples:num_train_samples+num_val_samples]
 val_grid_export = grid_export[num_train_samples:num_train_samples+num_val_samples]
-val_bat_expend = bat_expend[num_train_samples:num_train_samples+num_val_samples]
+val_bat_expend = bat_expend[num_train_samples:num_train_samples+num_val_samples] / 10
 
-test_consumption = consumption[num_train_samples+num_val_samples:]
-test_generation = generation[num_train_samples+num_val_samples:]
+test_forecast = forecast_data[num_train_samples+num_val_samples:]
 test_grid_import = grid_import[num_train_samples+num_val_samples:]
 test_grid_export = grid_export[num_train_samples+num_val_samples:]
 test_bat_expend = bat_expend[num_train_samples+num_val_samples:]
 
 # Build Model
 
-con = keras.Input(shape=(48, 1), name="consumption")
-gen = keras.Input(shape=(48, 1), name="generation")
+forecast = keras.Input(shape=(2, 48), name="forecast")
 
-features = layers.Concatenate()([con, gen])
-features = layers.LSTM(16, input_shape=(None, 48, 1))(features)
-features = layers.Dropout(0.5)(features)
-
-# g_imp = layers.Dense(48, activation="sigmoid", name="grid_import")(features)
-# g_exp = layers.Dense(48, activation="sigmoid", name="grid_export")(features)
-# b_exp = layers.Dense(48, activation="sigmoid", name="bat_expend")(features)
+features = layers.LSTM(48)(forecast)
 
 g_imp = layers.Dense(48, activation="relu", name="grid_import")(features)
 g_exp = layers.Dense(48, activation="relu", name="grid_export")(features)
-b_exp = layers.Dense(48, activation="relu", name="bat_expend")(features)
+b_exp = layers.Dense(48, activation="tanh", name="bat_expend")(features)
 
-
-
-model = keras.Model(inputs=[con, gen],
+model = keras.Model(inputs=forecast,
                     outputs=[g_imp, g_exp, b_exp])
 
-callbacks = [
-    keras.callbacks.ModelCheckpoint("hems",
-                                    save_best_only=True)
-]
 model.compile(optimizer="rmsprop",
               loss="mse",
               metrics=["mae"])
-model.fit([train_consumption, train_generation],
+model.fit([train_forecast],
           [train_grid_import, train_grid_export, train_bat_expend],
-          epochs=10,
-          validation_data=([val_consumption, val_generation], [val_grid_import, val_grid_export, val_bat_expend]),
-          callbacks=callbacks)
+          epochs=50,
+          validation_data=([val_forecast], [val_grid_import, val_grid_export, val_bat_expend]))
+grid_import_preds, grid_export_preds, bat_expend_preds = model.predict([test_forecast])
 
-
-
-# model.evaluate([val_consumption, val_generation],
-#                [val_grid_import, val_grid_export, val_bat_expend])
-
-grid_import_preds, grid_export_preds, bat_expend_preds = model.predict(
-    [test_consumption, test_generation])
+bat_expend_preds *= bat_expend_preds
 
 print(test_grid_import)
 print(grid_import_preds)
