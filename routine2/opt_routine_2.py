1006ebf26694f83e0bc82696ddd33a2e7b33b328
hamishmack1 <hamishmack01@gmail.com>
2023-09-28 22:28:04 +1000

    added lstm functions.

--- routine2/opt_routine_2.py
+++ routine2/opt_routine_2.py
@@ -34,7 +34,7 @@ def read_train_data(fname, base_path):
     consumption = np.zeros((length, len(header) - 2))
     generation = np.zeros((length, len(header) - 2))
     tariff = np.zeros((length, len(header) - 2))
-    bat_soc = np.zeros((length, 1))
+    bat_soc = np.zeros((length, len(header) - 2))
     grid_power = np.zeros((length, len(header) - 2))
 
     counter = 0
@@ -56,6 +56,55 @@ def read_train_data(fname, base_path):
             counter += 1
 
     return (length, consumption, generation, tariff, bat_soc, grid_power)
+
+
+def read_train_data_lstm(fname, base_path):
+    """Reads training data and intialises variables in a format suitable for
+        LSTM models.
+
+    Args:
+        fname: String specifying file name.
+        base_path: String specifying path where script is executed.
+
+    Returns:
+        2-d arrays for each variable respectively.
+    """
+
+    fname = os.path.join(base_path, "..", "training_data", fname)
+
+    with open(fname) as f:
+        data = f.read()
+
+    lines = data.split("\n")
+    header = lines[0].split(",")
+    lines = lines[1:]
+    length = int(len(lines) / 5)
+
+    # Inputs -> Forecast Data and battery SOC
+    # Outputs -> Grid Power
+
+    forecast_data = np.zeros((length, 4, len(header) - 2))
+    grid_power = np.zeros((length, len(header) - 2))
+
+    counter = 0
+    for line in lines:
+        split_line = line.split(",")
+        category = split_line[0]
+        values = [float(x) for x in split_line[2:]]
+
+        if category == "GC":
+            forecast_data[counter, 0, :] = values
+        elif category == "GG":
+            forecast_data[counter, 1, :] = values
+        elif category == "ToU":
+            forecast_data[counter, 2, :] = values
+        elif category == "SOC":
+            forecast_data[counter, 3, :] = values
+        elif category == "GP":
+            grid_power[counter, :] = values
+            counter += 1
+
+    return (length, forecast_data, grid_power)
 
 
 # Problem: Given current battery SOC, and forecast of demand and generation, predict todays optimal energy allocation decisions.
@@ -94,8 +143,8 @@ def prepare_data(data):
     val_tariff = tariff[num_train_samples:num_train_samples+num_val_samples]
     val_soc = bat_soc[num_train_samples:num_train_samples+num_val_samples]
     val_grid_power = grid_power[num_train_samples:num_train_samples+num_val_samples]
-    val_data = [val_consumption, val_generation, val_tariff, val_soc,
-                val_grid_power]
+    val_data = ([val_consumption, val_generation, val_tariff, val_soc],
+                val_grid_power)
 
     test_consumption = consumption[num_train_samples+num_val_samples:]
     test_generation = generation[num_train_samples+num_val_samples:]
@@ -108,8 +157,44 @@ def prepare_data(data):
     return train_data, val_data, test_data
 
 
-def build_pfa_model():
-    """Initialises policy function approximation (PFA) model using keras.
+def prepare_data_lstm(data):
+    """Separates training data into training, validation and testing sets
+        suitable for LSTM use.
+
+    Args:
+        length: Int specifying number of days in training data.
+        forecast_data: 3-d array specifying consumption, generation, tariff,
+                and SOC values over all time-steps.
+        grid_power: 2-d array specifying grid_power values over all time-steps.
+
+    Returns:
+        train_data: 4-d array containing training sets for each variable.
+        val_data: 4-d array containing validation sets for each variable.
+        test_data: 4-d array containing testing sets for each variable.
+    """
+
+    length, forecast_data, grid_power = data
+
+    num_train_samples = int(0.5 * length)
+    num_val_samples = int(0.25 * length)
+
+    train_forecast = forecast_data[:num_train_samples]
+    train_grid_power = grid_power[:num_train_samples]
+    train_data = [train_forecast, train_grid_power]
+
+    val_forecast = forecast_data[num_train_samples:num_train_samples+num_val_samples]
+    val_grid_power = grid_power[num_train_samples:num_train_samples+num_val_samples]
+    val_data = (val_forecast, val_grid_power)
+
+    test_forecast = forecast_data[num_train_samples+num_val_samples:]
+    test_grid_power = grid_power[num_train_samples+num_val_samples:]
+    test_data = [test_forecast, test_grid_power]
+    
+    return train_data, val_data, test_data
+
+
+def build_dense_pfa_model():
+    """Initialises policy function approximation (PFA) model using Dense layers.
 
     Returns:
         model: Initialised PFA model.
@@ -118,13 +203,15 @@ def build_pfa_model():
     con = keras.Input(shape=(48,), name="forecast")
     gen = keras.Input(shape=(48,), name="generation")
     tar = keras.Input(shape=(48,), name="tariff")
-    soc = keras.Input(shape=(1,), name="soc")
+    soc = keras.Input(shape=(48,), name="soc")
 
     features = layers.Concatenate()([con, gen, tar, soc])
+    features = layers.Dense(192)(features)
     features = layers.Dense(144)(features)
+    features = layers.Dense(96)(features)
     features = layers.Dropout(0.125)(features)
 
-    g_power = layers.Dense(48, activation="relu", name="grid_power")(features)
+    g_power = layers.Dense(48, name="grid_power")(features)
 
     model = keras.Model(inputs=[con, gen, tar, soc],
                         outputs=[g_power])
@@ -132,5 +219,27 @@ def build_pfa_model():
     model.compile(optimizer="rmsprop",
                 loss="mse",
                 metrics=["mae"])
+    
+    return model
+
+def build_lstm_pfa_model():
+    """Initialises policy function approximation (PFA) model using Dense layers.
+
+    Returns:
+        model: Initialised lstm PFA model.
+    """
+    forecast = keras.Input(shape=(4, 48), name="forecast")
+
+    features = layers.LSTM(192, recurrent_dropout=0.125)(forecast)
+    features = layers.Dropout(0.25)(features)
+
+    g_power = layers.Dense(48, name="grid_power")(features)
+
+    model = keras.Model(inputs=forecast,
+                        outputs=g_power)
+    
+    model.compile(optimizer="rmsprop",
+              loss="mse",
+              metrics=["mae"])
     
     return model
--- routine2/opt_routine_2_test.py
+++ routine2/opt_routine_2_test.py
@@ -8,18 +8,27 @@ from opt_routine_2 import *
 
 base_path = os.path.dirname(__file__)
 
-training_data = read_train_data("global.csv", base_path)
-train_data, val_data, test_data = prepare_data(training_data)
+# Dense model preparation
+# train_data, val_data, test_data = prepare_data(read_train_data("global.csv", base_path))
+# inputs = train_data[:4]
+# outputs = train_data[4]
 
-pfa_model = build_pfa_model()
+# dense_pfa_model = build_dense_pfa_model()
+# model = dense_pfa_model
 
-history = pfa_model.fit(train_data[:4],
-                        train_data[4],
-                        epochs=200,
-                        validation_data=(val_data[:4], val_data[4]))
+# LSTM model preparation
+train_data, val_data, test_data = prepare_data_lstm(read_train_data_lstm("global.csv", base_path))
+inputs = train_data[0]
+outputs = train_data[1]
+lstm_pfa_model = build_lstm_pfa_model()
+model = lstm_pfa_model
 
-grid_power = pfa_model.predict(test_data[:4])
+history = model.fit(inputs,
+                    outputs,
+                    epochs=300,
+                    validation_data=val_data)
+
+# grid_power = dense_pfa_model.predict(test_data[:4])
 
 mae_history = history.history["mae"]
 val_mae_history = history.history["val_mae"]
-
