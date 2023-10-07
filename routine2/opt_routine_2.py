1ab52a8e382024fd91d4fde1be34ecbaac2d08bd
hamishmack1 <hamishmack01@gmail.com>
2023-10-07 23:09:24 +1000

    completed initial pfa algorithm

--- routine2/opt_routine_2.py
+++ routine2/opt_routine_2.py
@@ -6,8 +6,14 @@ import numpy as np
 from pyomo.environ import *
 from tensorflow import keras
 from keras import layers
+from keras import activations
 from pandas import *
 import matplotlib.pyplot as plt
+
+timestep_headings = ["0:30","1:00","1:30","2:00","2:30","3:00","3:30","4:00","4:30","5:00",
+                    "5:30","6:00","6:30","7:00","7:30","8:00","8:30","9:00","9:30","10:00","10:30","11:00","11:30","12:00",
+                    "12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30",
+                    "19:00","19:30","20:00","20:30","21:00","21:30","22:00","22:30","23:00","23:30","0:00"]
 
 def read_train_data(fname, base_path):
 
@@ -17,11 +23,11 @@ def read_train_data(fname, base_path):
     fields = data.columns.to_list()
 
     timesteps = len(data.columns)
-    days = len(data) // 6
+    days = len(data) // 5
 
-    timestep_data = np.zeros((timesteps, days, 6))
+    timestep_data = np.zeros((timesteps, days, 5))
     for step in range(timesteps):
-        timestep_data[step] = data.iloc[:,step].to_numpy().reshape((days,6))
+        timestep_data[step] = data.iloc[:,step].to_numpy().reshape((days,5))
 
     return fields, timestep_data
 
@@ -35,7 +41,7 @@ def prepare_data(raw_data):
     train_data = raw_data[:num_train_samples]
     val_data = raw_data[num_train_samples:num_train_samples+num_val_samples]
     test_data = raw_data[num_train_samples+num_val_samples:]
- 
+
     return train_data, val_data, test_data
 
 
@@ -53,26 +59,29 @@ def build_model(train_data, val_data, model_name):
     soc = keras.Input(shape=(1,), name="soc")
 
     inputs = layers.Concatenate()([con, gen, tar, soc])
-    features = layers.Dense(4, activation="softmax")(inputs)
-    g_imp = layers.Dense(1, activation="relu", name="g_imp")(features)
-    g_exp = layers.Dense(1, activation="relu", name="g_exp")(features)
+
+    features = layers.Dense(256, activation="relu")(inputs)
+    features = layers.Dense(256, activation="relu")(features)
+    grid_power = layers.Dense(1, activation="linear", name="grid_power")(features)
 
     model = keras.Model(inputs=inputs,
-                        outputs=[g_imp, g_exp])
+                        outputs=[grid_power])
     
     model_path = os.path.join("timestep_models", model_name + ".keras")
     callbacks = [ keras.callbacks.ModelCheckpoint(model_path,
+                                                  monitor="val_loss",
                                                   save_best_only=True)
     ]
-    model.compile(optimizer="rmsprop",
-                loss="mse",
+    model.compile(optimizer="RMSprop",
+                loss=["mse"],
                 metrics=["mae"])
+
     history = model.fit(train_data[:,:4],
-                    [train_data[:,4], train_data[:,5]],
-                    epochs=500,
-                    validation_data=(val_data[:,:4], [val_data[:,4], val_data[:,5]]),
-                    callbacks=callbacks,
-                    verbose=1)
+                train_data[:,4],
+                epochs=100,
+                validation_data=(val_data[:,:4], val_data[:,4]),
+                callbacks=callbacks,
+                verbose=1)
     
     return history
 
@@ -85,3 +94,119 @@ def build_timestep_models(fname, base_path):
         build_model(train_data, val_data, step)
         model = keras.models.load_model("timestep_models/" + step + ".keras")
         print(f"Test MAE: {model.evaluate(test_data[:,:4], test_data[:,4])[1]:.3f}")
+
+def load_timestep_models():
+
+    models = []
+    for step in timestep_headings:
+        model = keras.models.load_model("timestep_models/" + step + ".keras")
+        models.append(model)
+        
+    return models
+
+def read_data(file_names, base_path):
+    """Reads historical/forecast data and initialises dependent variables.
+
+    Args:
+        file_names: Array of data files.
+        base_path: Path where script is executed.
+
+    Returns:
+        Arrays for independent and dependent variables respectively.
+    """
+    lines = []
+    for file in file_names:
+        fname = os.path.join(base_path, "..", "raw_data", file)
+
+        with open(fname) as f:
+            data = f.read()
+
+        line_data = data.split("\n")
+        header = line_data[0].split(",")
+        line_data = line_data[1:]
+
+        lines += line_data
+
+    # Parse data
+
+    length = int(len(lines) / 2)
+    consumption = np.zeros((length, len(header) - 5))
+    generation = np.zeros((length, len(header) - 5))
+    tou_tariff = np.zeros((length, len(header) - 5))
+    grid_power = np.zeros((length, len(header) - 5))
+    bat_charge = np.zeros((length, len(header) - 5))
+    bat_soc = np.zeros((length, len(header) - 5))
+
+    # ToU Pricing
+    
+    peak_tarrif = 0.539         # $AUD/kWh (3pm - 9pm)
+    offpeak_tarrif = 0.1495     # $AUD/kWh (All other times)
+    daily_import_cost = [offpeak_tarrif] * 29 + [peak_tarrif] * 13 + [offpeak_tarrif] * 6
+
+    date = []
+
+    counter = 0
+    for line in lines:
+        line_split = line.split(",")
+        category = line_split[3]
+        values = [float(x) for x in line_split[5:]]
+        if category == "GC":
+            consumption[counter] = values
+            tou_tariff[counter] = daily_import_cost
+            date.append(line_split[4])
+        elif category == "GG":
+            generation[counter] = values
+            counter += 1
+
+    days = None
+    return consumption[:days], generation[:days], tou_tariff[:days], \
+            grid_power[:days], bat_charge[:days], bat_soc[:days], date[:days]
+
+
+def plot_solution(consumption, generation, tou_tariff, grid_power, bat_charge,
+                  bat_soc, start, end, base_path):
+    """Plots optimal solution and saves as png file.
+
+    Args:
+        data: Arrays for all variables respectively.
+        start: Int specifying day to begin plot.
+        end: Int specifying day to end plot.
+        dec_horizon: String specifying decision horizon.
+        base_path: String specifying path where script is executed.
+    """
+
+    title = "Optimal SOC"
+
+    x_axis = range(len(consumption)*48)[start*48:end*48]
+    consumption_sliced = consumption.flatten()[start*48:end*48]
+    generation_sliced = generation.flatten()[start*48:end*48]
+    tou_tariff_sliced = tou_tariff.flatten()[start*48:end*48]
+    grid_power_sliced = grid_power.flatten()[start*48:end*48]
+    bat_charge_sliced = bat_charge.flatten()[start*48:end*48]
+    bat_soc_sliced = bat_soc.flatten()[start*48:end*48]
+
+    fig, ax1 = plt.subplots()
+    fig.set_size_inches(10,5)
+
+    ax1.set_xlabel("Time (0.5hrs)")
+    ax1.set_ylabel("Power (kW)")
+    ax1.plot(x_axis, consumption_sliced, label="consumption", linestyle='dashed')
+    ax1.plot(x_axis, generation_sliced, label="generation", linestyle='dashed')
+    ax1.plot(x_axis, grid_power_sliced, label="grid power")
+    ax1.plot(x_axis, bat_charge_sliced, label="bat charge")
+
+    ax1.set_title(title)
+
+    ax2 = ax1.twinx()
+
+    color = "tab:blue"
+    ax2.set_ylabel("Battery SOC (kWh)", color=color)
+    ax2.plot(x_axis, bat_soc_sliced, label="bat soc")
+    ax2.fill_between(x_axis, 0, 10, where=(tou_tariff_sliced == max(tou_tariff_sliced)),
+                     alpha=0.25, label="peak tariff", color="tab:grey")
+    ax2.tick_params(axis='y', labelcolor=color)
+
+    fig.tight_layout()
+    fig.legend(loc='upper left', fontsize="8")
+
+    fig.savefig(os.path.join(base_path, "results", title+".png"))
--- routine2/opt_routine_2_test.py
+++ routine2/opt_routine_2_test.py
@@ -8,22 +8,109 @@ from opt_routine_2 import *
 
 base_path = os.path.dirname(__file__)
 
-# build_timestep_models("global.csv", base_path)
+training_data = "global.csv"
 
-step_headings, data = read_train_data("global.csv", base_path)
+historical_data = ["Customer2(2010-2011).csv",
+                    "Customer2(2011-2012).csv",
+                    "Customer2(2012-2013).csv"]
 
-train_data, val_data, test_data = prepare_data(data[18])
-history = build_model(train_data, val_data, step_headings[18])
-model = keras.models.load_model("timestep_models/" + step_headings[18] + ".keras")
-print(f"Test MAE: {model.evaluate(test_data[:,:4], test_data[:,4:6])[1]:.3f}")
+# build_timestep_models(training_data, base_path)
+
+timestep_models = load_timestep_models()
+
+consumption, generation, tou_tariff, grid_power, bat_charge, bat_soc, date = read_data(historical_data, base_path)
+
+generation *= 4.075
+
+days = range(len(consumption))
+timesteps = range(len(timestep_headings))
+
+# Set initial battery SOC
+bat_soc[0,0] = 2
+
+for day in days:
+    print("Day: " + day)
+    for step in timesteps:
+
+        inputs = [[consumption[day,step], generation[day,step], tou_tariff[day,step], bat_soc[day,step]]]
+        grid_power[day,step] = timestep_models[step].predict(inputs)[0]
+        bat_charge[day,step] = generation[day,step] - consumption[day,step] + grid_power[day,step]
+        
+        if step < 47:
+            if bat_charge[day,step] <= 0:
+                bat_soc[day,step+1] = bat_soc[day,step] + (1/0.9)*bat_charge[day,step]
+
+                if bat_soc[day,step+1] < 2:
+                    difference = 2 - bat_soc[day,step+1]
+                    bat_charge[day,step] += difference
+                    grid_power[day,step] += difference
+                    bat_soc[day,step+1] = 2
+
+            else:
+                bat_soc[day,step+1] = bat_soc[day,step] + 0.9*bat_charge[day,step]
+
+                if bat_soc[day,step+1] > 10:
+                    difference = bat_soc[day,step+1] - 10
+                    bat_charge[day,step] -= difference
+                    grid_power[day,step] -= difference
+                    bat_soc[day,step+1] = 10
+        else:
+            if bat_charge[day,step] <= 0:
+                bat_soc[day+1,0] = bat_soc[day,step] + (1/0.9)*bat_charge[day,step]
+
+                if bat_soc[day+1,0] < 2:
+                    difference = 2 - bat_soc[day,step+1]
+                    bat_charge[day,step] += difference
+                    grid_power[day,step] += difference
+                    bat_soc[day+1,0] = 2
+
+            else:
+                bat_soc[day+1,0] = bat_soc[day,step] + 0.9*bat_charge[day,step]
+
+                if bat_soc[day+1,0] > 10:
+                    difference = bat_soc[day,step+1] - 10
+                    bat_charge[day,step] -= difference
+                    grid_power[day,step] -= difference
+                    bat_soc[day+1,0] = 10
+
+    if day == 108:
+        break
+
+plot_solution(consumption, generation, tou_tariff, grid_power, bat_charge,
+                  bat_soc, 100, 107, base_path)
 
 
-grid_power_preds = model.predict(test_data[:,:4])
-print(test_data[:,4:6])
-print(grid_power_preds)
 
-# print(test_data[:,4])
-# print(grid_power_preds)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# step_headings, data = read_train_data("global.csv", base_path)
+
+# train_data, val_data, test_data = prepare_data(data[18])
+# history = build_model(train_data, val_data, step_headings[18])
+# model = keras.models.load_model("timestep_models/" + step_headings[18] + ".keras")
+
+# stats = model.evaluate(test_data[:,:4], test_data[:,4])
+# print(f"Test MAE: {stats[1]:.3f}")
+
+# grid_power_preds = model.predict(test_data[:,:4])
 
 # mae_history = history.history["mae"]
 # val_mae_history = history.history["val_mae"]
@@ -31,19 +118,40 @@ print(grid_power_preds)
 # plt.plot(range(1, len(mae_history) + 1), mae_history, label="training")
 # plt.plot(range(1, len(val_mae_history) + 1), val_mae_history, label="validation")
 # plt.xlabel("Epochs")
-# plt.ylabel("MAE")
+# plt.ylabel("MAE (kW)")
+# plt.title("9:30 Timestep")
 # plt.legend()
 # plt.show()
 
-start = 0
-end = None
+# start = 0
+# end = None
 
-gi = grid_power_preds[0].flatten()[start:end] - grid_power_preds[1].flatten()[start:end] 
-gi_real = test_data[:,4][start:end] - test_data[:,5][start:end]
+# grid_power_preds = grid_power_preds.flatten()
+# for i in range(len(grid_power_preds)):
+#     if abs(grid_power_preds[i]) < 0.002:
+#         grid_power_preds[i] = 0
 
-plt.plot(range(len(gi)), gi, label="Prediction")
-plt.plot(range(len(gi)), gi_real, label="Actual")
-plt.xlabel("Time")
-plt.ylabel("Power (kWh)")
-plt.legend()
-plt.show()
+# gi = grid_power_preds[start:end] 
+# gi_real = test_data[:,4][start:end]
+
+# plt.plot(range(len(gi)), gi, label="Prediction")
+# plt.plot(range(len(gi)), gi_real, label="Actual")
+# plt.xlabel("Time")
+# plt.ylabel("Power (kWh)")
+# plt.legend()
+# plt.show()
+
+# function to add value labels
+# def addlabels(x,y):
+#     for i in range(len(x)):
+#         plt.text(i, y[i], round(y[i], 3), ha = 'center')
+
+# fig = plt.figure(figsize = (10, 5))
+ 
+# # creating the bar plot
+# plt.bar(units_str, evals, width=0.5)
+# addlabels(units_str, evals)
+ 
+# plt.xlabel("Units")
+# plt.ylabel("Test MAE (kW)")
+# plt.show()
